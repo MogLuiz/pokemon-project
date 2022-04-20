027fe5d2f15b91feaf35fc2312b3ea8e23377754
Luiz Henrique <luizhjramos@outlook.com>
2022-04-19 21:25:04 -0300

    test: should render correctly with default props

--- src/components/EmptyResult/__test__/EmptyResult.test.js
+++ src/components/EmptyResult/__test__/EmptyResult.test.js
@@ -1,1 +1,22 @@
-// TODO testar component
+// Packages
+import React from "react";
+import { render } from "@testing-library/react";
+
+// Components
+import EmptyResult from "../EmptyResult";
+
+describe("EmptyResult", () => {
+  test("should render correctly with default props", () => {
+    const { container, getByAltText, getByText } = render(<EmptyResult />);
+
+    const defaultMessage = "Oops... Não encontramos nada.";
+    const defaultWidth = 200;
+
+    const image = getByAltText(/empty result/i);
+
+    expect(container).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+    expect(getByText(defaultMessage)).toBeInTheDocument();
+    expect(image.width).toBe(defaultWidth);
+  });
+});
