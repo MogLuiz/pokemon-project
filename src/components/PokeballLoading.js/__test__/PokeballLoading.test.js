5d0cc9d83a2e09f557a30fb5328a26dcca2b8f25
Luiz Henrique <luizhjramos@outlook.com>
2022-04-19 22:19:45 -0300

    test: should render correctly with default props

--- src/components/PokeballLoading.js/__test__/PokeballLoading.test.js
+++ src/components/PokeballLoading.js/__test__/PokeballLoading.test.js
@@ -1,1 +1,21 @@
-// TODO testar component
+// Packages
+import React from "react";
+import { render } from "@testing-library/react";
+
+// Components
+import PokeballLoading from "../PokeballLoading";
+
+describe("PokeballLoading", () => {
+  test("should render correctly with default props", () => {
+    const { getByAltText, container } = render(<PokeballLoading />);
+
+    const defaultSize = 200;
+    const image = getByAltText(/pokeball loading/i);
+
+    expect(container).toBeInTheDocument();
+    expect(image).toBeInTheDocument();
+
+    expect(image.width).toBe(defaultSize);
+    expect(image.height).toBe(defaultSize);
+  });
+});
