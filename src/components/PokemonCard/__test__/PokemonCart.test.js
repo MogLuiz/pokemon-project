55ac40ed52223b0400d1994e444bdbaa386ff906
Luiz Henrique <luizhjramos@outlook.com>
2022-05-01 18:58:46 -0300

    test(PokemonCard): should render with default props

--- src/components/PokemonCard/__test__/PokemonCart.test.js
+++ src/components/PokemonCard/__test__/PokemonCart.test.js
@@ -7,19 +7,26 @@ import { cardBuilder } from "../../../__mocks__/card-builder";
 
 // Components
 import PokemonCard from "../PokemonCard";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
 
 const setup = (props = { card: cardBuilder({ name: "MeuPojemon" }) }) => {
   const { card } = props;
   const onClick = jest.fn();
 
+  const imageAlt = `${card.id}-${card.name}`;
+
   const renderResult = render(<PokemonCard {...card} onClick={onClick} />);
 
-  const imageElement = renderResult.getByAltText(`${card.id}-${card.name}`);
+  const imageElement = renderResult.getByAltText(imageAlt);
 
-  return { card: card, ...renderResult, imageElement };
+  return { card: card, ...renderResult, imageElement, imageAlt };
 };
 describe("PokemonCard", () => {
   it("should render with default props", () => {
-    const container = render(<PokemonCard />);
+    const { imageElement, card, imageAlt } = setup();
+
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement.src).toBe(card.imageUrl);
+    expect(imageElement.alt).toBe(imageAlt);
   });
 });
